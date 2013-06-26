a81d052e97b19043697368996fcca7f42de1c8ef
Krasu <krasniyrus@gmail.com>
2013-06-26 14:43:55 +0400

    now sending form to heroku
    fixes #36, fixes #35, fixes #34

--- js/main.js
+++ js/main.js
@@ -49,4 +49,97 @@ $(function () {
             }
         }
     });
-});
+
+
+    $.fn.contactForm = function () {
+        var form = this,
+            regEmail = /[\w\.+]{1,}[@][\w\-]{1,}([.]([\w\-]{1,})){1,5}$/ig,
+            regPhone = /[^0-9]/g,
+            fields = $('input,textarea', form),
+            submit = $('[type="submit"]', form)
+
+        submit.on('click', function (event) {
+            event.preventDefault()
+            disableForm()
+
+            fields.each(function (i, field) {
+                field = $(field)
+
+                !validateField(field) && setFieldError(field);
+            })
+
+            if (fields.filter('.error').length) return enableForm();
+
+            sendForm()
+        })
+
+        function sendForm() {
+            $.post('http://murmuring-ravine-4226.herokuapp.com/', {
+                name: fields.filter('[name=name]').val(),
+                email: fields.filter('[name=email]').val(),
+                phone: fields.filter('[name=phone]').val(),
+                text: fields.filter('[name=text]').val()
+            }, 'json')
+                .done(function (data) {
+                    if (data.success) {
+                        fields.add(submit).hide()
+                        form.find('.success-message').show()
+                        return
+                    }
+
+                    $.each(data.errors, function (key) {
+                        setFieldError(fields.filter('[name=' + key + ']'))
+                    })
+
+                    enableForm()
+                })
+                .fail(function (xhr) {
+                    enableForm(xhr.responseText)
+                })
+        }
+
+        function setFieldError(field) {
+            field.addClass('error')
+            field.one('keyup blur', function () {
+                field.removeClass('error')
+
+                if (!fields.filter('.error').length) {
+                    $('.error-message', form).hide()
+                }
+            });
+        }
+
+        function validateField(field) {
+            var type = field.attr('type'),
+                val = $.trim(field.val())
+
+            if (type == 'email') return regEmail.test(val);
+
+            if (type == 'tel') return val.replace(regPhone, '');
+
+            return val.length > 0;
+        }
+
+        function disableForm(message) {
+            message = message || 'loading'
+            submit.data('label', submit.html());
+            submit.html(message + '...');
+            fields.add(submit).prop('disabled', true);
+            $('.error-message', form).hide()
+        }
+
+        function enableForm(error) {
+            error = error || 'Check your data please'
+            submit.html(submit.data('label'));
+            fields.add(submit).prop('disabled', false)
+            fields.filter('.error').first().focus()
+
+            $('.error-message', form).html(error).show()
+        }
+
+        return this
+    }
+
+    $('#contactus form').contactForm()
+})
+;
