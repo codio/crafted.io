0e0b6942e20317687f746a706bd5d17712eeebe4
Joel Moss <joel@developwithstyle.com>
2014-01-27 14:33:27 +0000

    Updated production deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,6 @@
+# config valid only for Capistrano 3.1
+lock '3.1.0'
+
 set :application, 'crafted-site'
 set :repo_url, 'https://github.com/codio/crafted.io.git'
 set :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,4 @@
-server 'ec2-54-234-79-66.compute-1.amazonaws.com', :app, :web, :db, :primary => true
-server 'ec2-184-73-51-70.compute-1.amazonaws.com', :app, :web, :db
+set :stage, :production
+
+server 'web1.int.codio.com', user: 'deploy', roles: %w{web app db}
+server 'web2.int.codio.com', user: 'deploy', roles: %w{web app db}
