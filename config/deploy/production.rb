fb689c1e853deddaac9c95acfa593f4c9d938999
Joel Moss <joel@developwithstyle.com>
2015-01-05 14:54:29 +0000

    Using new webheads

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :stage, :production
 
-server 'web3.int.codio.com', user: 'capistrano', roles: %w{web app db}
-server 'web4.int.codio.com', user: 'capistrano', roles: %w{web app db}
+server 'web1.int.codio.com', user: 'capistrano', roles: %w{web app db}
+server 'web2.int.codio.com', user: 'capistrano', roles: %w{web app db}
